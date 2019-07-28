e4f996e15a235c703ccad4b432146c9b89413541
Denny Huang <denny0223@gmail.com>
2019-07-29 00:27:06 +0800

    Change to python telegram bot

--- app/send_scanner.py
+++ app/send_scanner.py
@@ -1,15 +1,14 @@
 import json
 
 import config
-import telepot
 
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Bot, InlineKeyboardMarkup, InlineKeyboardButton
 
 keyboard = InlineKeyboardMarkup(inline_keyboard=[[
     InlineKeyboardButton(text='掃下去', callback_game=True)
 ]])
 
-bot = telepot.Bot(config.BOT_TOKEN)
+bot = Bot(config.BOT_TOKEN)
 
 teams = []
 with open('teams.json', 'r') as teams_json:
